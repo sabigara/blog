041744c91e174ceec84ce49e7cd60f9ca2c39b69
Matsura Yuma <lemonburst1958@gmail.com>
2022-02-19 17:25:17 +0900

    Initial commit

---
+++ data/projectsData.ts
@@ -0,0 +1,10 @@
+const projectsData = [
+  {
+    title: 'Reaktr',
+    description: 'Reaktr enables you to generate audio-reactive music video with ease.',
+    imgSrc: '/static/images/reaktr.jpeg',
+    href: 'https://reaktr.vercel.app',
+  },
+]
+
+export default projectsData
---
+++ data/siteMetadata.js
@@ -0,0 +1,31 @@
+const siteMetadata = {
+  title: 'Studio Rubik',
+  author: 'Matsura Yuma',
+  headerTitle: 'Studio Rubik',
+  description: 'A blog created with Next.js and Tailwind.css',
+  language: 'en-us',
+  theme: 'system', // system, dark or light
+  siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
+  siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
+  siteLogo: '/static/images/logo.png',
+  image: '/static/images/avatar.png',
+  socialBanner: '/static/images/twitter-card.png',
+  email: 'lemonburst1958@gmail.com',
+  github: 'https://github.com/sabigara',
+  twitter: 'https://twitter.com/_sabigara',
+  facebook: '',
+  youtube: '',
+  linkedin: '',
+  locale: 'en-US',
+  analytics: {
+    plausibleDataDomain: '', // e.g. tailwind-nextjs-starter-blog.vercel.app
+  },
+  newsletter: {
+    provider: '',
+  },
+  comment: {
+    provider: '',
+  },
+}
+
+module.exports = siteMetadata
