233786bec0f35c3dc1fbf0106440919c047657d4
Matsura Yuma <lemonburst1958@gmail.com>
2022-08-03 13:16:43 +0900

    Add projects

--- components/Card.tsx
+++ components/Card.tsx
@@ -6,7 +6,7 @@ const Card = ({ title, description, imgSrc, href }) => (
     <div
       className={`${
         imgSrc && 'h-full'
-      }  overflow-hidden rounded-xl border-2 border-gray-200 border-opacity-60 dark:border-gray-700`}
+      }  flex flex-col overflow-hidden rounded-xl border-2 border-gray-200 border-opacity-60 dark:border-gray-700`}
     >
       {imgSrc &&
         (href ? (
@@ -28,8 +28,8 @@ const Card = ({ title, description, imgSrc, href }) => (
             height={306}
           />
         ))}
-      <div className="p-6">
-        <h2 className="mb-3 text-2xl font-bold leading-8 tracking-tight">
+      <div className="flex flex-1 flex-col p-6">
+        <h2 className="mb-3 text-2xl font-bold leading-8 tracking-tight text-gray-800 dark:text-gray-200">
           {href ? (
             <Link href={href} aria-label={`Link to ${title}`} noExternalIcon>
               {title}
@@ -38,14 +38,16 @@ const Card = ({ title, description, imgSrc, href }) => (
             title
           )}
         </h2>
-        <p className="prose mb-3 max-w-none text-gray-500 dark:text-gray-400">{description}</p>
+        <p className="prose mb-3 max-w-none flex-1 text-sm leading-6 text-gray-600 dark:text-gray-300">
+          {description}
+        </p>
         {href && (
           <Link
             href={href}
-            className="text-base font-medium leading-6 text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
+            className="w-fit text-base font-medium leading-6 text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
             aria-label={`Link to ${title}`}
           >
-            Learn more
+            Visit
           </Link>
         )}
       </div>
