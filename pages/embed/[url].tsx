fb8d0e8e4ba63091517ad996ec9c87e73d15b395
Matsura Yuma <lemonburst1958@gmail.com>
2022-08-03 22:47:26 +0900

    Support dark theme

--- pages/embed/[url].tsx
+++ pages/embed/[url].tsx
@@ -35,7 +35,7 @@ export default function EmbeddedPage({ siteMetadata }: Props) {
       href={siteMetadata.url}
       target="_blank"
       rel="noopener noreferrer nofollow"
-      className="grid h-32 w-full grid-cols-6 grid-rows-6 overflow-clip rounded-lg border-[1px] transition-colors hover:bg-slate-50"
+      className="grid h-32 w-full grid-cols-6 grid-rows-6 overflow-clip rounded-lg border-[1px] transition-colors hover:bg-slate-50 dark:border-gray-600 dark:hover:bg-gray-800"
     >
       <div
         className="col-span-4 row-span-6 grid grid-rows-3 p-4"
