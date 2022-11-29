f23d92e4ac3ea5545ec8210da13551862ddc7cf5
Matsura Yuma <lemonburst1958@gmail.com>
2022-11-29 15:15:01 +0900

    Modify "copy code" button

--- components/Pre.tsx
+++ components/Pre.tsx
@@ -1,4 +1,5 @@
 import { useState, useRef, ReactNode } from "react"
+import { BiCopy, BiCheck } from "react-icons/bi"
 
 interface Props {
   children: ReactNode
@@ -30,40 +31,10 @@ const Pre = ({ children }: Props) => {
         <button
           aria-label="Copy code"
           type="button"
-          className={`absolute right-2 top-2 h-8 w-8 rounded border-2 bg-gray-700 p-1 dark:bg-gray-800 ${
-            copied
-              ? "border-green-400 focus:border-green-400 focus:outline-none"
-              : "border-gray-300"
-          }`}
+          className={`absolute right-2 top-2 h-8 w-8 p-1 text-gray-100`}
           onClick={onCopy}
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            fill="none"
-            className={copied ? "text-green-400" : "text-gray-300"}
-          >
-            {copied ? (
-              <>
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-6 9l2 2 4-4"
-                />
-              </>
-            ) : (
-              <>
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2"
-                />
-              </>
-            )}
-          </svg>
+          {copied ? <BiCheck className="h-6 w-6" /> : <BiCopy className="h-6 w-6" />}
         </button>
       )}
 
