5e8dde739206b5abc0bf0201b880644d3d479ce8
Matsura Yuma <lemonburst1958@gmail.com>
2022-11-30 14:48:16 +0900

    Modify headerNavLinks

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,8 +1,8 @@
 const headerNavLinks = [
   { href: "/blog", title: "Blog" },
-  { href: "/tags", title: "Tags" },
   { href: "/projects", title: "Projects" },
   { href: "/about", title: "About" },
+  { href: "/feed.xml", title: "RSS" },
 ]
 
 export default headerNavLinks
