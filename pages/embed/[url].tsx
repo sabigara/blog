b00044e0d6ea44b386bb7634d94b35cc4f0ce697
Matsura Yuma <lemonburst1958@gmail.com>
2022-12-02 01:50:50 +0900

    Add nofollow, noindex to /embed/[url]

--- pages/embed/[url].tsx
+++ pages/embed/[url].tsx
@@ -1,3 +1,4 @@
+import Head from "next/head"
 import { faviconUrl } from "@/lib/site-metadata/faviconUrl"
 import { getSiteMetadata, SiteMetadata } from "@/lib/site-metadata/getSiteMetadata"
 import { undefinedFieldsToNull } from "@/lib/utils/object"
@@ -52,46 +53,51 @@ export default function EmbeddedPage({ siteMetadata }: Props) {
   const domain = extractDomain(siteMetadata.url)
 
   return (
-    <a
-      href={siteMetadata.url}
-      target="_blank"
-      rel="noopener noreferrer nofollow"
-      data-no-external-icon
-      className="grid h-32 w-full grid-cols-6 grid-rows-6 overflow-clip rounded-lg border-[1px] transition-colors hover:bg-slate-50 dark:border-gray-600 dark:hover:bg-gray-800"
-    >
-      <div
-        className="col-span-4 row-span-6 grid items-start justify-center gap-1 overflow-hidden p-4"
-        style={{ placeContent: "center normal" }}
+    <>
+      <Head>
+        <meta name="robots" content="nofollow, noindex" />
+      </Head>
+      <a
+        href={siteMetadata.url}
+        target="_blank"
+        rel="noopener noreferrer nofollow"
+        data-no-external-icon
+        className="grid h-32 w-full grid-cols-6 grid-rows-6 overflow-clip rounded-lg border-[1px] transition-colors hover:bg-slate-50 dark:border-gray-600 dark:hover:bg-gray-800"
       >
-        <h1 className="font-bold line-clamp-2">{siteMetadata.title || siteMetadata.ogp.title}</h1>
-        <p className="truncate text-sm text-gray-500">
-          {siteMetadata.description || siteMetadata.ogp.description}
-        </p>
-        <p className="flex items-center gap-2 truncate text-sm">
-          {/* eslint-disable-next-line @next/next/no-img-element */}
-          <img
-            src={
-              siteMetadata.url.startsWith("https://rubiq.vercel.app")
-                ? "/static/favicons/favicon-32x32.png"
-                : faviconUrl(siteMetadata.url)
-            }
-            alt="ファビコン"
-            width="16"
-            height="16"
-          />
-          {domain}
-        </p>
-      </div>
-      <div className="col-span-2 row-span-6">
-        {siteMetadata.ogp.image && (
-          // eslint-disable-next-line @next/next/no-img-element
-          <img
-            src={siteMetadata.ogp.image}
-            className="ml-auto h-full object-cover"
-            alt="ページの画像"
-          />
-        )}
-      </div>
-    </a>
+        <div
+          className="col-span-4 row-span-6 grid items-start justify-center gap-1 overflow-hidden p-4"
+          style={{ placeContent: "center normal" }}
+        >
+          <h1 className="font-bold line-clamp-2">{siteMetadata.title || siteMetadata.ogp.title}</h1>
+          <p className="truncate text-sm text-gray-500">
+            {siteMetadata.description || siteMetadata.ogp.description}
+          </p>
+          <p className="flex items-center gap-2 truncate text-sm">
+            {/* eslint-disable-next-line @next/next/no-img-element */}
+            <img
+              src={
+                siteMetadata.url.startsWith("https://rubiq.vercel.app")
+                  ? "/static/favicons/favicon-32x32.png"
+                  : faviconUrl(siteMetadata.url)
+              }
+              alt="ファビコン"
+              width="16"
+              height="16"
+            />
+            {domain}
+          </p>
+        </div>
+        <div className="col-span-2 row-span-6">
+          {siteMetadata.ogp.image && (
+            // eslint-disable-next-line @next/next/no-img-element
+            <img
+              src={siteMetadata.ogp.image}
+              className="ml-auto h-full object-cover"
+              alt="ページの画像"
+            />
+          )}
+        </div>
+      </a>
+    </>
   )
 }
