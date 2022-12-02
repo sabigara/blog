df78f3df544022dff1d116702cbe9a25e884a892
Matsura Yuma <lemonburst1958@gmail.com>
2022-12-02 16:20:04 +0900

    Remove privacy policy page

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,13 +1,6 @@
-import Link from "next/link"
-
 export default function Footer() {
   return (
     <footer className="flex flex-col items-center gap-1 bg-slate-100 py-2 text-gray-500">
-      <div>
-        <Link href="/privacy" className="text-xs hover:text-gray-900 hover:underline">
-          Privacy policy
-        </Link>
-      </div>
       <small className="text-xs">
         Copyright © {new Date().getFullYear()} Matsura Yuma. All rights reserved.
       </small>
