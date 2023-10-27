c7c25b84d7a6ba93f9a711ac8aea67923f4bb4ad
Matsura Yuma <41217985+sabigara@users.noreply.github.com>
2023-10-27 22:17:02 +0900

    faviconを追加

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ja">
+      <head>
+        <link href="/icon.svg" rel="icon" type="image/svg+xml" />
+        <link href="/favicon.ico" rel="icon" sizes="any" />
+        <link href="/apple-touch-icon.png" rel="apple-touch-icon" />
+        <link href="/manifest.webmanifest" rel="manifest" />
+      </head>
       <body className={inter.className}>
         <Header />
         <main className="min-h-screen container px-container">{children}</main>
