659dc1796357692051a5c8bd9282420779f24bdf
Matsura Yuma <41217985+sabigara@users.noreply.github.com>
2023-10-30 17:43:13 +0900

    postの初期ファイル生成を自動化

---
+++ plopfile.mjs
@@ -0,0 +1,41 @@
+import { exec } from "child_process";
+import { nanoid } from "nanoid";
+
+function fn(plop) {
+  plop.setHelper("today", () => {
+    return new Date().toISOString().split("T")[0];
+  });
+
+  plop.setActionType("open", function (answers) {
+    const filePath = `src/content/posts/${answers.slug}.mdx`;
+    exec(`code ${filePath}`, (error) => {
+      if (error) {
+        console.error("Failed to open the file in VSCode:", error);
+      }
+    });
+  });
+
+  plop.setGenerator("post", {
+    description: "Blog post",
+    prompts: [
+      {
+        type: "input",
+        name: "slug",
+        message: "filename that will be used as the slug for the post",
+        default: () => nanoid(8),
+      },
+    ],
+    actions: [
+      {
+        type: "add",
+        path: "src/content/posts/{{slug}}.mdx",
+        templateFile: "templates/post.mdx.hbs",
+      },
+      {
+        type: "open",
+      },
+    ],
+  });
+}
+
+export default fn;
