4cc342caff9c7a2c7a74387f40fd234fd3bcf6d9
Matsura Yuma <41217985+sabigara@users.noreply.github.com>
2023-10-30 09:48:06 +0900

    next.config.redirectsを追加

--- next.config.js
+++ next.config.js
@@ -5,6 +5,20 @@ const nextConfig = {
   experimental: {
     scrollRestoration: true,
   },
+  async redirects() {
+    return [
+      {
+        source: "/(en|ja)/:path*",
+        destination: "/:path*",
+        permanent: true,
+      },
+      {
+        source: "/blog/:path*",
+        destination: "/posts/:path*",
+        permanent: true,
+      },
+    ];
+  },
   webpack: (config) => {
     config.module.rules.push({
       test: /\.svg$/i,
