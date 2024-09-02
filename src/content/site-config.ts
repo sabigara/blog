f493f06d440567b339f93a9169929e01a212923b
Matsura Yuma <41217985+sabigara@users.noreply.github.com>
2024-09-02 15:18:43 +0900

    Production環境のフォント配信先をR2に移行

--- src/content/site-config.ts
+++ src/content/site-config.ts
@@ -1,6 +1,10 @@
+const isProd = process.env.NODE_ENV === "production";
+
+const staticUrl = isProd
+  ? "https://static.sabigara.com"
+  : "http://localhost:3700";
+
 export const siteConfig = {
-  url:
-    process.env.NODE_ENV === "production"
-      ? "https://sabigara.com"
-      : "http://localhost:3700",
+  url: isProd ? "https://sabigara.com" : "http://localhost:3700",
+  fontUrl: `${staticUrl}/fonts/NotoSansJP-Bold.ttf`,
 } as const;
